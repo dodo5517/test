dfc0c62208c6f9f48eeb469ef68e875806f48279
dodo5517 <kh9690754@naver.com>
2024-07-25 10:20:44 +0900

    소켓 연결

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express"; //express 모듈 가져옴
 import path from "path"; //path 모듈 가져옴 -> __dirname 사용 가능.
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express(); //express 인스턴스 생성
 const port = 3000; //3000번에서 열거임.
+const httpServer = createServer(app); //http 서버 생성
+
 const __dirname = path.resolve(); //절대 경로 저장.
 
 import chatRouter from "./chat/chat.js"; //라우터로 분리한 라우터모듈을 가져옴.
@@ -13,12 +17,33 @@ app.get("/", (req, res) => {
 
 app.use("/chat", chatRouter); // http://localhost:port/chat으로 오는 모든 요청(use)은 chatRouter가 처리할 거임.
 
-app.listen(port, () => {
-  console.log(`
-    ###############################################
-    Server listening on port: ${port}
-    ###############################################
-    `);
+//웹소켓 서버 생성
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:" + port,
+  },
 });
 
+let user = {};
+
+//연결하면
+io.on("connection", (socket) => {
+  console.log("새로운 사용자가 연결되었습니다.");
+  user[socket.id] = {
+    id: socket.id,
+  };
+
+  //연결을 끊는다면
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`
+    ###############################################
+            Server listening on port: ${port}
+    ###############################################
+         `);
+});
 // console.log("dirname : " + __dirname);
